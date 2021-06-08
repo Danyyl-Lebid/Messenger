181a8d226c4a2e14d4d5be80a8e02c9125f89f94
Danyyl-Lebid <64252230+Danyyl-Lebid@users.noreply.github.com>
2021-06-08 23:49:25 +0300

    Strategies are lambdas now

--- core/src/main/java/com/github/messenger/handlers/http/HttpHandlerStrategyMap.java
+++ core/src/main/java/com/github/messenger/handlers/http/HttpHandlerStrategyMap.java
@@ -1,9 +1,7 @@
 package com.github.messenger.handlers.http;
 
 import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -12,7 +10,7 @@ public class HttpHandlerStrategyMap {
 
     private final Map<String, IHttpHandlerStrategy> map = new HashMap<>();
 
-    private final HttpHandlerStrategyContext context;
+    private HttpHandlerStrategyContext context;
 
     private static final String NICKNAME = "Nickname";
 
@@ -20,30 +18,28 @@ public class HttpHandlerStrategyMap {
 
     public HttpHandlerStrategyMap(HttpHandlerStrategyContext context) {
         this.context = context;
+        this.initializeMap();
+    }
+
+    private void initializeMap() {
         this.map.put("/messenger/auth", authorizationStrategy);
         this.map.put("/messenger/reg", registrationStrategy);
     }
 
-    private final IHttpHandlerStrategy authorizationStrategy = new IHttpHandlerStrategy() {
-        @Override
-        public void doHandle(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-            ServletOutputStream out = resp.getOutputStream();
-            String body = req.getReader().lines().collect(Collectors.joining());
-            Map<String, String> result = context.getAuthorizationController().authorize(body);
-            resp.setHeader(NICKNAME, result.get(NICKNAME));
-            resp.setContentType("application/json");
-            resp.setStatus(HttpServletResponse.SC_OK);
-            out.write(result.get(TOKEN).getBytes());
-        }
+    private final IHttpHandlerStrategy authorizationStrategy = (req, resp) -> {
+        ServletOutputStream out = resp.getOutputStream();
+        String body = req.getReader().lines().collect(Collectors.joining());
+        Map<String, String> result = context.getAuthorizationController().authorize(body);
+        resp.setHeader(NICKNAME, result.get(NICKNAME));
+        resp.setContentType("application/json");
+        resp.setStatus(HttpServletResponse.SC_OK);
+        out.write(result.get(TOKEN).getBytes());
     };
 
-    private final IHttpHandlerStrategy registrationStrategy = new IHttpHandlerStrategy() {
-        @Override
-        public void doHandle(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-            String body = req.getReader().lines().collect(Collectors.joining());
-            context.getRegistrationController().register(body);
-            resp.setStatus(HttpServletResponse.SC_OK);
-        }
+    private final IHttpHandlerStrategy registrationStrategy = (req, resp) -> {
+        String body = req.getReader().lines().collect(Collectors.joining());
+        context.getRegistrationController().register(body);
+        resp.setStatus(HttpServletResponse.SC_OK);
     };
 
     public Map<String, IHttpHandlerStrategy> getMap() {
--- core/src/main/java/com/github/messenger/handlers/websocket/WebsocketHandlerStrategyMap.java
+++ core/src/main/java/com/github/messenger/handlers/websocket/WebsocketHandlerStrategyMap.java
@@ -1,18 +1,15 @@
 package com.github.messenger.handlers.websocket;
 
 import com.github.messenger.dto.HistoryRequestDto;
-import com.github.messenger.payload.Envelope;
-import com.github.messenger.payload.Token;
 import com.github.messenger.payload.Topic;
 import com.github.messenger.utils.JsonHelper;
 
-import javax.websocket.Session;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WebsocketHandlerStrategyMap {
 
-    private final WebsocketHandlerStrategyContext context;
+    private WebsocketHandlerStrategyContext context;
 
     private final Map<Topic, IWebsocketHandlerStrategy> map = new HashMap<>();
 
@@ -20,7 +17,7 @@ public class WebsocketHandlerStrategyMap {
         this.context = context;
     }
 
-    private void initializeMap(){
+    private void initializeMap() {
         this.map.put(Topic.LOGIN, loginStrategy);
         this.map.put(Topic.GLOBAL_MESSAGE, globalMessageStrategy);
         this.map.put(Topic.MESSAGE, messageStrategy);
@@ -33,89 +30,57 @@ public class WebsocketHandlerStrategyMap {
         this.map.put(Topic.LOGOUT, logoutStrategy);
     }
 
-    private final IWebsocketHandlerStrategy loginStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
-            context.getGlobalConnectionPool().addSession(token.getUserId(), session);
-            context.getRoomConnectionPools().addSession(token.getUserId(), session);
-            context.getStatusController().setOnline(token.getUserId());
-            String response = JsonHelper.toJson(envelope).orElseThrow();
-            context.getBroker().broadcast(context.getGlobalConnectionPool().getSessions(), response);
-        }
+    private final IWebsocketHandlerStrategy loginStrategy = (session, envelope, token) -> {
+        context.getGlobalConnectionPool().addSession(token.getUserId(), session);
+        context.getRoomConnectionPools().addSession(token.getUserId(), session);
+        context.getStatusController().setOnline(token.getUserId());
+        String response = JsonHelper.toJson(envelope).orElseThrow();
+        context.getBroker().broadcast(context.getGlobalConnectionPool().getSessions(), response);
     };
 
-    private final IWebsocketHandlerStrategy globalMessageStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
-            context.getGlobalMessageController().save(token.getUserId(), envelope.getPayload());
-            String response = JsonHelper.toJson(envelope).orElseThrow();
-            context.getGlobalMessageController().broadcast(response);
-        }
+    private final IWebsocketHandlerStrategy globalMessageStrategy = (session, envelope, token) -> {
+        context.getGlobalMessageController().save(token.getUserId(), envelope.getPayload());
+        String response = JsonHelper.toJson(envelope).orElseThrow();
+        context.getGlobalMessageController().broadcast(response);
     };
 
-    private final IWebsocketHandlerStrategy messageStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
-            context.getMessageController().save(token.getUserId(), envelope.getPayload());
-            context.getMessageController().broadcast(envelope);
-        }
+    private final IWebsocketHandlerStrategy messageStrategy = (session, envelope, token) -> {
+        context.getMessageController().save(token.getUserId(), envelope.getPayload());
+        context.getMessageController().broadcast(envelope);
     };
 
-    private final IWebsocketHandlerStrategy chatHistoryStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
-            HistoryRequestDto historyRequestDto = JsonHelper.fromJson(envelope.getPayload(), HistoryRequestDto.class).orElseThrow();
-            context.getMessageController().sendHistory(session, historyRequestDto.getChatId());
-        }
+    private final IWebsocketHandlerStrategy chatHistoryStrategy = (session, envelope, token) -> {
+        HistoryRequestDto historyRequestDto = JsonHelper.fromJson(envelope.getPayload(), HistoryRequestDto.class).orElseThrow();
+        context.getMessageController().sendHistory(session, historyRequestDto.getChatId());
     };
 
-    private final IWebsocketHandlerStrategy globalHistoryStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
+    private final IWebsocketHandlerStrategy globalHistoryStrategy = (session, envelope, token) ->
             context.getGlobalMessageController().sendHistory(session);
-        }
-    };
 
-    private final IWebsocketHandlerStrategy createChatStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
+    private final IWebsocketHandlerStrategy createChatStrategy = (session, envelope, token) ->
             context.getChatController().createChat(envelope.getPayload());
-        }
+
+    private final IWebsocketHandlerStrategy nicknamesStrategy = (session, envelope, token) -> {
+        String response = context.getChatController().getAllUsers();
+        context.getBroker().sendAsync(session, response);
     };
 
-    private final IWebsocketHandlerStrategy nicknamesStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
-            String response = context.getChatController().getAllUsers();
-            context.getBroker().sendAsync(session, response);
-        }
+    private final IWebsocketHandlerStrategy participantsStrategy = (session, envelope, token) -> {
+        String response = context.getChatController().getParticipants(envelope.getPayload());
+        context.getBroker().sendAsync(session, response);
     };
 
-    private final IWebsocketHandlerStrategy participantsStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
-            String response = context.getChatController().getParticipants(envelope.getPayload());
-            context.getBroker().sendAsync(session, response);
-        }
+    private final IWebsocketHandlerStrategy chatsStrategy = (session, envelope, token) -> {
+        String response = context.getChatController().getChats(token.getUserId());
+        context.getBroker().sendAsync(session, response);
     };
 
-    private final IWebsocketHandlerStrategy chatsStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
-            String response = context.getChatController().getChats(token.getUserId());
-            context.getBroker().sendAsync(session, response);
-        }
-    };
-
-    private final IWebsocketHandlerStrategy logoutStrategy = new IWebsocketHandlerStrategy() {
-        @Override
-        public void doHandle(Session session, Envelope envelope, Token token) {
-            context.getGlobalConnectionPool().removeSession(token.getUserId());
-            context.getRoomConnectionPools().removeSession(token.getUserId());
-            context.getStatusController().setOffline(token.getUserId());
-            String response = JsonHelper.toJson(envelope).orElseThrow();
-            context.getBroker().broadcast(context.getGlobalConnectionPool().getSessions(), response);
-        }
+    private final IWebsocketHandlerStrategy logoutStrategy = (session, envelope, token) -> {
+        context.getGlobalConnectionPool().removeSession(token.getUserId());
+        context.getRoomConnectionPools().removeSession(token.getUserId());
+        context.getStatusController().setOffline(token.getUserId());
+        String response = JsonHelper.toJson(envelope).orElseThrow();
+        context.getBroker().broadcast(context.getGlobalConnectionPool().getSessions(), response);
     };
 
     public Map<Topic, IWebsocketHandlerStrategy> getMap() {
